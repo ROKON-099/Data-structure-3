fd3b9ec0d1d2996ea67c278850e45f026e0dbcd6
Rokon007 <rokon@example.com>
2026-03-10 20:04:55 +0600

    first commit

---
+++ P1.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    int n;
+    cout << "Enter the lenght of Array:";
+    cin >>n;
+    int arr[n];
+    cout << "Enter " <<n << " Number of Array:";
+    for (int i=0; i<n; i++)
+    {
+
+
+        cin >>arr[i];
+    }
+    int largest=arr[0], smallest=arr[0];
+    for (int i=0; i<n; i++)
+    {
+        if (arr[i]> largest)
+            largest =arr[i];
+        if (arr[i]< smallest)
+            smallest =arr[i];
+
+    }
+    cout << "The largest array is :"<<largest <<endl;
+    cout << "The smallest array is :" <<smallest <<endl;
+
+
+
+
+
+
+
+
+}
